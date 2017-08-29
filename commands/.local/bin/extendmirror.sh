b7225855ae6ef031d6f18528f6399734293a6d51
acgtyrant <acgtyrant@gmail.com>
2017-08-29 22:44:29 +0800

    extendmirror.sh: update

--- commands/.local/bin/extendmirror.sh
+++ commands/.local/bin/extendmirror.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xrandr --output eDP1 --mode 1366x768 --output HDMI1 --mode 1920x1080 --right-of eDP1
-xrandr --output HDMI1 --primary
+#xrandr --output eDP1 --mode 1366x768 --output HDMI1 --mode 1920x1080 --right-of eDP1
+xrandr --output DVI-D-0 --primary
 feh --bg-fill '/home/acgtyrant/background'
