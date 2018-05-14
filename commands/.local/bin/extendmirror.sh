52da86273261309df7eed0a426bd95c7b9dd700f
acgtyrant <acgtyrant@gmail.com>
2018-05-14 14:42:55 +0800

    extendmirror.sh: update for home

--- commands/.local/bin/extendmirror.sh
+++ commands/.local/bin/extendmirror.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#xrandr --output eDP1 --mode 1366x768 --output HDMI1 --mode 1920x1080 --right-of eDP1
+xrandr --output HDMI-0 --mode 1440x900 --left-of DVI-D-0
 xrandr --output DVI-D-0 --primary
 feh --bg-fill '/home/acgtyrant/background'
