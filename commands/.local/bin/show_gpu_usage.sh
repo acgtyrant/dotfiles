37ca84cc82db8cda993debc7b56aa81c9ca04d9f
acgtyrant <acgtyrant@gmail.com>
2018-10-23 17:52:00 +0800

    show_gpu_usage.sh: remove zz

--- commands/.local/bin/show_gpu_usage.sh
+++ commands/.local/bin/show_gpu_usage.sh
@@ -3,7 +3,7 @@
 # Simplify Your Life With an SSH Config File
 # http://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/
 
-for server in aa yy zz dd tuba 102 103
+for server in aa yy dd tuba 102 103
 do
   echo $server
   timeout 3s ssh $server nvidia-smi pmon -c 1
